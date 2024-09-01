8c1b95fb3e9e6bc03c03d20606e4bfbd243726e3
bruce <lucsack@gmail.com>
2024-09-01 18:39:46 +0200

    might as well install tpm for tmux

--- bin/bin/install_packer.sh
+++ bin/bin/install_packer.sh
@@ -2,4 +2,4 @@
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
